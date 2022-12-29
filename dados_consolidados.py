6f589393847993c075874b7c4920fa53a7059b34
Tiago-Amarante <tadbc1@gmail.com>
2022-12-29 15:18:51 -0300

    atualizaçao

--- dados_consolidados.py
+++ dados_consolidados.py
@@ -1,15 +1,19 @@
 print("O Programa começou a rodar")
 
-import undetected_chromedriver as uc
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 from time import sleep
 
-options = webdriver.ChromeOptions() 
+options = Options()
+prefs = {'download.default_directory' : 'C:\\Users\\tadbc\\OneDrive\\Área de Trabalho\\codigos\\Multimarcas\\webscrapping-bacen\\dados_rar'}
+options.add_experimental_option('prefs', prefs)
 options.headless = True
-driver = uc.Chrome(options=uc.options.ChromeOptions())
-for ano in reversed(range(23)):
+driver = webdriver.Chrome(options=options)
+for ano in reversed(range(22,23)):
     print(ano)
     for mes in reversed(range(13)):
+        print(mes)
         if 0 < ano < 10 and 0 < mes < 10:
           driver.get(f'https://www.bcb.gov.br/Fis/Consorcios/Port/BD/200{ano}0{mes}Consorcios.zip')
           sleep(2)
